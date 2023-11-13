6472ac5dcf5029f105db3fa207a3b9ae4c326dc4
Daniel Lam <Dann.lam@gmail.com>
2023-11-13 15:09:36 -0800

    Swapped to onlyAudio

--- borderify.js
+++ borderify.js
@@ -2,5 +2,12 @@ document.body.style.border = "5px solid red";
 
 let player = document.querySelector(".ytp-right-controls");
 
+player.style.border = "5px solid red";
+
+let tempButton = document.createElement("button");
+tempButton.class = "ytp-button";
+tempButton.style.border = "5px solid blue";
+
+player.appendChild(tempButton);
 console.log(`Object Keys: ${Object.keys(player)}`);
 console.log(`Object values: ${Object.values(player)}`);
