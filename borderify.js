6db43944b6fcf53e6b19f53826353fbfa3b27563
Daniel Lam <Dann.lam@gmail.com>
2023-11-13 14:24:43 -0800

    First commit

---
+++ borderify.js
@@ -0,0 +1,6 @@
+document.body.style.border = "5px solid red";
+
+let player = document.querySelector(".ytp-right-controls");
+
+console.log(`Object Keys: ${Object.keys(player)}`);
+console.log(`Object values: ${Object.values(player)}`);
