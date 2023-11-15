05941afe383621a2d7a87b51250faa4a01f7a74c
Daniel Lam <Dann.lam@gmail.com>
2023-11-15 11:14:05 -0800

    Updated button to better fit YT theme

--- js/global.js
+++ js/global.js
@@ -1,26 +1,33 @@
 "use strict";
-console.log("Hello!");
+
+//targetTabID is used to hold sets of tab IDs and associate audio URLs for them.
 const targetTabId = new Set();
+//initializes variable for use in multiple functions
 var enable;
 
+//Check in local storage whether our extension is enabled or not
 chrome.storage.local.get("youtube_audio_state", (values) => {
   enable = values.youtube_audio_state;
   enable ? enableExtension() : disableExtension();
 });
 
+//Listen for a push on the extension enabler, sync up its enable or disable with whether it's toggled or not, reload the tab if you disable the extension.
 chrome.browserAction.onClicked.addListener(() => {
   enable = !enable;
   enable ? enableExtension() : (disableExtension(), reloadTab());
   chrome.storage.local.set({ youtube_audio_state: enable });
 });
 
+//This receives a message that will be sent once Audio streams are detected, records which tabs have grabbable audio streams.
 chrome.runtime.onMessage.addListener((message, sender) => {
   targetTabId.add(sender.tab.id);
   console.log(`targetTabIds: ${[...targetTabId]}`);
 });
 
+//If you remove a tab, remove that tab from our set.
 chrome.tabs.onRemoved.addListener((tabId) => targetTabId.delete(tabId));
 
+//Receives a loooong URL with parameters, essentially removing parameters listed later and splits the URL into one that requests the audio stream.
 function removeURLParameters(url, parametersToBeRemoved) {
   const urlparts = url.split("?");
   if (urlparts.length >= 2) {
@@ -40,6 +47,7 @@ function removeURLParameters(url, parametersToBeRemoved) {
   return url;
 }
 
+//Reloads all tabs listed in our tab set, if they're active.
 function reloadTab() {
   for (const TabId of targetTabId) {
     chrome.tabs.get(TabId, (tab) => {
@@ -51,7 +59,7 @@ function reloadTab() {
   }
 }
 
-// can't cancel video or video without audio-only stream will not play
+// The meat and potatoes of the script. Processes the targettab from youtube to find the audio stream.
 function processRequest(details) {
   //   console.log("originUrl = " + details.originUrl + "\n");
   //   console.log("documentUrl = " + details.documentUrl + "\n");
@@ -66,23 +74,34 @@ function processRequest(details) {
   //   console.log("details.url= " + details.url);
   //   console.log("details.type= " + details.type);
   if (details.url.includes("mime=audio") && !details.url.includes("live=1")) {
+    //Searches for a URL that has the audio stream instead of a video stream, exclude live streamed YT content.
+
     // reverse parameter order (same as url parameter traversal order)
-    console.log("unremovedURL = " + details.url + "\n");
+
+    // console.log("unremovedURL = " + details.url + "\n");
+
+    //Parameters to remove from the URL to request an audio stream.
     const parametersToBeRemoved = ["ump", "rbuf=", "rn=", "range="];
 
     const audioURL = removeURLParameters(details.url, parametersToBeRemoved);
+    //Sends a message with our tab ID and associates it with the audio stream.
+    //Subsequent message Listeners receive this and point at their associated tabs.
     chrome.tabs.sendMessage(details.tabId, { url: audioURL });
-    console.log("AudioURL is: " + audioURL + "\n");
+    // console.log("AudioURL is: " + audioURL + "\n");
   }
 }
 
+//Once we enable the extension, listen for xmlhttprequests.
 function enableExtension() {
+  //Sets the icon
   chrome.browserAction.setIcon({
     path: {
       128: "img/icon128.png",
       38: "img/icon38.png",
     },
   });
+
+  //Once the extension is enabled, it listens for all URLs that are utilizing an xmlhttprequest, and gives it an option to block, right now we're not utilizing the block, but potentially down the line it could be useful for future updates.
   chrome.webRequest.onBeforeRequest.addListener(
     processRequest,
     { urls: ["<all_urls>"], types: ["xmlhttprequest"] },
@@ -90,6 +109,7 @@ function enableExtension() {
   );
 }
 
+//Removes listener and sets ICON to a greyed out one once disabled.
 function disableExtension() {
   chrome.browserAction.setIcon({
     path: {
--- js/youtube_audio.js
+++ js/youtube_audio.js
@@ -1,47 +1,45 @@
 "use strict";
-//Build a button
-//Append it to our player
-//Have that button control whether videoElement.src = URL.
-//If the button is "On", then change videoElement.src to our URL.
-//Otherwise, leave videoElement.src
-//Might need to "get" the currentTime of it, and then send that to our video, to then play it at the correct time.
-//May need to consider whether this is an asynchronous action.
-//Maybe I send the message once the button is hit, instead of sending the message straight up from the backend?
 
+//Selects the right controllers on the youtube player.
 let rightController = document.querySelector(".ytp-right-controls");
 
+//Creates a button for YT player,
 let audioOnlyButton = document.createElement("button");
-audioOnlyButton.class = "ytp-button";
-// Set inline styles for the button
-// tempButton.style.backgroundColor = "#ff0000"; // YouTube red color
-// tempButton.style.color = "#ffffff"; // White text color
-// tempButton.style.padding = "5px 5px"; // Adjust padding as needed
-audioOnlyButton.style.borderRadius = "3px";
+//Set class and additional styling.
+//This is a temporary stopgap, ideally I have a style sheet connecting to this.
+audioOnlyButton.className = "ytp-fullscreen-button ytp-button";
+
 audioOnlyButton.style.cursor = "pointer";
-// tempButton.style.transition = "background-color 0.3s";
-audioOnlyButton.style.width = "30px";
-audioOnlyButton.style.height = "30px";
-audioOnlyButton.style.verticalAlign = "top";
+audioOnlyButton.style.width = "40px";
+audioOnlyButton.style.height = "40px";
+
+//Insert our button to the far left of our controller.
 rightController.insertBefore(audioOnlyButton, rightController.firstChild);
 
+//Creates the SVG Icon for our player.
 let svgDiv = document.createElement("svg");
 
-svgDiv.innerHTML = `<svg
-width="100%"
-height="100%"
-viewBox="0 0 36 36"
-xmlns="http://www.w3.org/2000/svg"
-fill="none"
-stroke="#000000"
-stroke-width="1"
-stroke-linecap="round"
-stroke-linejoin="miter"
->
-<rect x="2" y="5" width="14" height="14" rx="0"></rect>
-<polyline points="16 10 22 7 22 17 16 14" stroke-linecap="round"></polyline>
-</svg>`;
+//Describes the icon for our player.
 
+svgDiv.innerHTML = `<svg width="100%" height="100%" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.0098 9C21.6589 9.86548 22.0098 10.9181 22.0098 12C22.0098 13.0819 21.6589 14.1345 21.0098 15" stroke="#FFFFFF" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M22 2L2 22" stroke="#FFFFFF" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M9.21094 19H14.0709C15.1318 19 16.1492 18.5788 16.8994 17.8286C17.6495 17.0785 18.0709 16.0609 18.0709 15V10.1902" stroke="#FFFFFF" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.3203 6.68018C16.9577 6.16238 16.4759 5.73942 15.9155 5.44702C15.355 5.15462 14.7325 5.00119 14.1004 5H6.02034C4.96817 5.01315 3.96357 5.44048 3.2242 6.18921C2.48483 6.93793 2.07025 7.94774 2.07033 9V15C2.06763 15.8994 2.36814 16.7736 2.92336 17.4812C3.47857 18.1888 4.25607 18.6888 5.13033 18.9001" stroke="#FFFFFF" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`;
+{
+  /* <svg width="100%" height="100%" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.0098 9C21.6589 9.86548 22.0098 10.9181 22.0098 12C22.0098 13.0819 21.6589 14.1345 21.0098 15" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M22 2L2 22" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M9.21094 19H14.0709C15.1318 19 16.1492 18.5788 16.8994 17.8286C17.6495 17.0785 18.0709 16.0609 18.0709 15V10.1902" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M17.3203 6.68018C16.9577 6.16238 16.4759 5.73942 15.9155 5.44702C15.355 5.15462 14.7325 5.00119 14.1004 5H6.02034C4.96817 5.01315 3.96357 5.44048 3.2242 6.18921C2.48483 6.93793 2.07025 7.94774 2.07033 9V15C2.06763 15.8994 2.36814 16.7736 2.92336 17.4812C3.47857 18.1888 4.25607 18.6888 5.13033 18.9001" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg> */
+}
+
+//Append the icon to our button.
 audioOnlyButton.appendChild(svgDiv);
+
+//Listens for a click on our new button, swaps the src for the youtube player to our audio stream that was found in global.js
 let videoURL;
 audioOnlyButton.addEventListener("click", function () {
   const videoElement = document.getElementsByTagName("video")[0];
@@ -51,10 +49,12 @@ audioOnlyButton.addEventListener("click", function () {
   }
 });
 
-chrome.runtime.sendMessage("1");
+// Once the message is sent, it receives the URL which we assign to videoURL to be sent to the player.
+
+// chrome.runtime.sendMessage("1");
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   videoURL = request.url;
-  console.log(`URL from YT_AUD is: ${videoURL}`);
+  // console.log(`URL from YT_AUD is: ${videoURL}`);
   // console.dir(Object.getPrototypeOf(videoElement));
   // console.log(`videoElement src is: ${videoElement.src}`);
   // console.log(`videoElement.currentTime: ${videoElement.currentTime}`);
